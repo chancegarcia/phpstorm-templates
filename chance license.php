14a55648fbdd91d993c7d1d189f2f02bab9003be
chance garcia <develop@chancegarcia.com>
2013-03-08 14:59:15 -0500

    adding subpackage to license and base files

--- chance license.php
+++ chance license.php
@@ -1,5 +1,7 @@
 /**
  * @package     ${Package}
+#if(${Subpackage} != "") * @subpackage  ${Subpackage}
+#end
  * @author      Chance Garcia
  * @copyright   (C)Copyright ${YEAR} chancegarcia.com
  */
